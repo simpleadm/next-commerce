e85667e8649232d6851f2f8d86cfee3b9d907c89
Sergey <sergey@convert.no>
2022-09-05 09:34:56 +0300

    updates from upstream

--- site/components/common/UserNav/UserNav.tsx
+++ site/components/common/UserNav/UserNav.tsx
@@ -46,7 +46,7 @@ const UserNav: React.FC<{
               variant="naked"
               onClick={() => {
                 setSidebarView('CART_VIEW')
-                toggleSidebar()
+                openSidebar()
               }}
               aria-label={`Cart items: ${itemsCount}`}
             >
@@ -88,8 +88,8 @@ const UserNav: React.FC<{
             aria-label="Menu"
             variant="naked"
             onClick={() => {
-              openSidebar()
               setSidebarView('MOBILE_MENU_VIEW')
+              openSidebar()
             }}
           >
             <Menu />
